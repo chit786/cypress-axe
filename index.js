4502f7aa5c9a4f7f3b75d0613f6ec03b60af536d
Chitrang Natu <chit786@gmail.com>
2019-10-15 09:54:53 +0200

    refactor dependencies

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import addContext from 'mochawesome/addContext';
+import addContext from '../mochawesome/addContext';
 
 Cypress.Commands.add('injectAxe', () => {
   cy.window({ log: false }).then(window => {
